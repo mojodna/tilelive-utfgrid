8ceaca60e861d9fd125ba8815282886cf99bd35d
Erik Vullings <erik.vullings@gmail.com>
2016-02-26 11:33:13 +0100

    Added tilelive-tmstyle example (for Mapbox Studio Classic project)
    * Added example/tmstyle.js
    * Added test/fixtures/road.tm2 Mapbox Studio Classic example project: 
    NOTE that all paths in tmstyle.js, data.yml and project.yml are hardcoded, and willl only work without change if your installation path is c:\dev\web\tilelive-utfgrid
    * Updated package.json references (old versions couldn't be installed on Windows)
    * Added typings/node/node.d.ts for Intellisense support in vscode.

--- examples/mbtiles.js
+++ examples/mbtiles.js
@@ -8,7 +8,19 @@ var UTFGrid = require("../")(tilelive);
 MBTiles.registerProtocols(tilelive);
 UTFGrid.registerProtocols(tilelive);
 
-tilelive.load("utfgrid+mbtiles://../test/fixtures/plain_2.mbtiles", function(err, source) {
+// Paths that work on Windows
+// c:/dev/web/tilelive-utfgrid/test/fixtures/plain_2.mbtiles
+// c/dev/web/tilelive-utfgrid/test/fixtures/plain_2.mbtiles
+// /dev/web/tilelive-utfgrid/test/fixtures/plain_2.mbtiles
+// process.cwd() + "/test/fixtures/plain_2.mbtiles
+// __dirname + "/../test/fixtures/plain_2.mbtiles
+//
+// Doesn't work (SQLITE_CANTOPEN: unable to open database file)
+// ../test/fixtures/plain_2.mbtiles (original)
+// c:\dev\web\tilelive-utfgrid\test\fixtures\plain_2.mbtiles
+// test/fixtures/plain_2.mbtiles
+
+tilelive.load("utfgrid+mbtiles://" + __dirname + "/../test/fixtures/plain_2.mbtiles", function(err, source) {
   if (err) {
     console.error(err.stack);
     process.exit(1);
